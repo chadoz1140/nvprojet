30606cb0a34bcb62e682f695e658978121f3105f
chadoz1140 <125833730+chadoz1140@users.noreply.github.com>
2025-04-04 19:55:24 +0200

    Add files via upload

---
+++ monScript.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+/* Le code de mon bouton "Open" qui sert de menu explicite vers les différentes éléments demandé */
